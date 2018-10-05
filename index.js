32c8ff4af390f67d1c9dc599c79730fc0fb54ca8
NimiBahar <nimrodbahar@gmail.com>
2018-10-05 21:53:00 +0300

    parsing payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer( (req, res) => {
 
@@ -18,13 +19,26 @@ const server = http.createServer( (req, res) => {
 
     //Get the headers as an object
     const headers = req.headers;
-    
-    //Send the response
-    res.end('Hello world\n');
 
-    //Log the request path
-    console.log(`Got a ${httpMethod} request on path: ${trimmedPath} with the query string`, queryStringObject);
-    console.log(`Got a req with this headers: `, headers);
+    //Get the payload if any
+    const stringDecoder = new StringDecoder('utf-8');
+
+    let buffer = '';
+
+    req.on('data', (data) => {
+        buffer += stringDecoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += stringDecoder.end();
+
+        //Send the response
+        res.end('Hello world\n');
+
+        //Log the request path
+        //console.log(`Got a ${httpMethod} request on path: ${trimmedPath} with the query string`, queryStringObject);
+        //console.log(`Got a req with this headers: `, headers);
+        console.log(`Got a req with this payload: `, buffer);
+        })
 });
 
 server.listen(3000, () => { console.log('Server is up an running on port 3000') })
